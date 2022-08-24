ff41ced281d2d3d83772a7076d8ac900fbc7a5df
Harley Johnson <hjohnson@uwm.com>
2022-08-24 09:29:14 -0400

    Initial commit - setup.

---
+++ src/redux/reducers.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+// import { UserReducer as user } from "../common/components/user/redux";
+// import { LayoutReducer as layout } from '../common/components/layout/MainLayout/redux';
+
+const rootReducer = combineReducers({
+  // user,
+  // layout,
+});
+
+export default rootReducer;
